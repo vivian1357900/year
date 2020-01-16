18a602ef809e101bf386b04351b3d3b36c44004a
vivian1357900 <59930201+vivian1357900@users.noreply.github.com>
2020-01-16 17:43:10 +0800

    Add files via upload

---
+++ math_14.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf ("%d", &a);
+    if ((a%4 == 0 && a%100 != 0) || a%400 == 0)
+        printf ("Bissextile Year\n");
+    else printf ("Common Year\n");
+    return 0;
+}
